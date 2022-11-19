3fd60c68f9b8b6fcc7e107eafd47b1175f29ecca
Jeet Barot <jeetbarot1998@gmail.com>
2022-11-19 22:14:16 +0530

    Update Service_twitter_nlp.py

--- services/Service_twitter_nlp.py
+++ services/Service_twitter_nlp.py
@@ -1,7 +1,7 @@
 from flask_restx import Resource
 from models.twitter_nlp_model import api, Twitter_Payload
 from utilities.cache import cache
-from bl.twitter_nlp import main_func
+from bl.twitter_nlp import fetch_tweets_analysis
 import json
 
 @api.route('/analyse')
@@ -11,5 +11,5 @@ class Follow(Resource):
     @api.doc(security='apikey')
     @api.response('default', 'Error')
     def post(self):
-        response = main_func(api.payload['keyword'], api.payload['number_of_tweets'])
+        response = fetch_tweets_analysis(api.payload['keyword'], api.payload['number_of_tweets'])
         return response
